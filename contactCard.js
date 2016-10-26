839be804ff7dfbb48915cccbcfe3946cfa35bc18
jtinahara <jt.inahara@gmail.com>
2016-10-26 14:53:20 -0700

    Add files via upload

--- contactCard.js
+++ contactCard.js
@@ -3,19 +3,17 @@ $(document).ready(function(){
 		var first = $('#first').val(),
 		last = $('#last').val(),
 		desc = $('#description').val();
+		
 
+		$('first, last, desc').val()
 		$('#contactCards').append('<div class="cards"><h2>'+first+'&nbsp;'+last+'</h2><p>'+desc+'</p></div>');
+		$('#first, #last, #description').val('');
+		$('#first').focus();
 		return false;
 		});
 		
 	$(document).on('click', '.cards', function(){
 		$('p, h2').toggle();
-	
-
-	});
-
-	$('.cards').click(function(){
-		('p').fadeIn(800, function());
 	});
 
 });
